3429087d1c2432cad5cfc60b2640df536e9dccee
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2024-06-10 17:23:14 +0000

    version inicial

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('index.html')
+    x1 = float(request.form.get('valor1',0))
+    x2 = float(request.form.get('valor2',0))
+    y = float(request.form.get('resultado',0))
+
+    if request.method == 'POST':
+        y = x1 + x2
+
+    return render_template('index.html', valor1=x1, valor2=x2, resultado=y, nombre='Mauricio')
 
 if __name__ == '__main__':
     app.run(debug=True)
